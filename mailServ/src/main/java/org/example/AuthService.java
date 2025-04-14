f453a7f975e17163938b567cd5d9fb846413ddb4
Herz Elarbi <herzelarbi@gmail.com>
2025-04-14 20:53:15 +0100

    here we created interface that it's methodes should be implemented later on

---
+++ mailServ/src/main/java/org/example/AuthService.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface AuthService extends Remote {
+    boolean authenticate(String username, String password) throws RemoteException;
+    boolean createUser(String username, String password) throws RemoteException;
+    boolean deleteUser(String username) throws RemoteException;
+    boolean updatePassword(String username, String newPassword) throws RemoteException;
+}
