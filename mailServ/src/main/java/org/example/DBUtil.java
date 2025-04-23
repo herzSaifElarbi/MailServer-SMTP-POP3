a21bedb976273709ccf1b040269fd79e7685bad0
herzSaifElarbi <herzelarbi@gmail.com>
2025-04-23 19:41:36 +0100

    TP4:creating DBUtil used to connct to our db using user, pass

---
+++ mailServ/src/main/java/org/example/DBUtil.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+import java.sql.*;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/mail_server?useSSL=false";
+    private static final String USER = "root"; // adjust this
+    private static final String PASS = "Amin@07032001"; // adjust this
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL driver not found", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
